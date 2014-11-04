d4f074e8322ed10246c6b9cfec2f94b2fcf83663
Ian Go <igo@andrew.cmu.edu>
2014-11-03 19:32:50 -0500

    added index, update, delete

--- models/pasta.js
+++ models/pasta.js
@@ -1,36 +1,62 @@
+
+//constructor of menu class, defines pasta object 
 function Menu(){
 	this.menu = [];
-}
 
-var Pasta = function(name, noodle, sauce){
+	var Pasta = function(name, noodle, sauce){
 	this.name = name;
 	this.noodle = noodle; 
 	this.sauce = sauce; 
-};
+	};
 
+}
+
+//returns menu array of pastas
 Menu.prototype.getMenu = function(){
 	return this.menu;
 }
 
+//returns pasta object searched by name
 Menu.prototype.findPasta = function(name){
-	for(int i = 0; i<this.menu.length; i++)
+	for(var i = 0; i<this.menu.length; i++)
 	{
-		if(menu[i].name==name)
+		if(this.menu[i].name == name)
 		{
-			return menu[i];
+			return this.menu[i];
 		}
 	}
 }
 
+//returns index of pasta object searched by name
+Menu.prototype.indexOfPasta = function(name){
+	return this.menu.indexOf(this.findPasta(name));
+}
+
+//creates a new pasta object and inserts into array
 Menu.prototype.createPasta = function(name, noodle, sauce){
 	var tempPasta = new Pasta(name,noodle,sauce);
 	this.menu.push(tempPasta);
+	return this.getMenu();
 }
 
-// Menu.protoype.findPasta = function(name){
-// 	for menu
-// }
+//deletes pasta object by name
+Menu.prototype.deletePasta = function(name){
+	var index = this.indexOfPasta(name);
+	if(index > -1){
+		this.menu.splice(index,1);
+	}
+	return this.getMenu();
+}
+
+//updates pasta object in array by name
+Menu.prototype.updatePasta = function(oldName, newName, newNoodle, newSauce){
+	var tempPasta = new Pasta(newName,newNoodle,newSauce);
+	var index = this.indexOfPasta(oldName);
+
+	this.menu[index]=tempPasta;
+
+	return this.getMenu();
+}
 
 module.exports.menu;
-module.exports = Pasta; 
 
